8758d9f6236226e3cfb52ebe8db299556c40b0c6
davta101 <chemsfexs@gmail.com>
2024-08-03 19:23:35 +0400

    add: info

--- src/constants/index.js
+++ src/constants/index.js
@@ -25,21 +25,36 @@ export const navbar = [
 ]
 
 
-export const InfoText =[
+export const InfoText = [
     {
-        h3:"Personal info",
-        p:"Please provide your name, email address, and phone number.",
-        li1:"Name",
-        li2:"Email Address",
-        li3:"Phone Number",
-    
-    }
+        h3: "Personal info",
+        p: "Please provide your name, email address, and phone number.",
+    },
+    {
+        h3: "Name",
+        label: "Name",
+        plHolder: "Enter your name...",
+        type: "text"
+    },
+    {
+        h3: "Email",
+        label: "Email",
+        plHolder: "Enter email...",
+        type: "Email"
+
+    },
+    {
+        h3: "Number",
+        label: "Number",
+        plHolder: "Enter your number...",
+        type: "number"
+    },
 ]
 
-export const selectText=[
+export const selectText = [
     {
-        h3:"Select your plan",
-        p:"you have th option of monthly or year billing.",
+        h3: "Select your plan",
+        p: "you have th option of monthly or year billing.",
     }
 ]
 
@@ -47,28 +62,28 @@ export const selectText=[
 export const SelectDiv = [
 
     {
-        img:Arcade,
-        span:"Arcade",
-        money:"$9/mo"
+        img: Arcade,
+        span: "Arcade",
+        money: "$9/mo"
     },
     {
-        img:Advanced,
-        span:"Advanced",
-        money:"$12/mo"
+        img: Advanced,
+        span: "Advanced",
+        money: "$12/mo"
     },
     {
 
-        img:Pro,
-        span:"Pro",
-        money:"$15/mo"
+        img: Pro,
+        span: "Pro",
+        money: "$15/mo"
     }
-    
+
 ]
 
-export const OnsctText =[
+export const OnsctText = [
     {
-        h3:"Pick add-ons",
-        pm:"Add-ons help enhance your gaming experience.a",
+        h3: "Pick add-ons",
+        pm: "Add-ons help enhance your gaming experience.a",
 
 
     }
@@ -77,29 +92,29 @@ export const OnsctText =[
 export const OnsctDiv = [
 
     {
-        img:Check,
-        p:"Online service",
-        p1:"Access to multiplayer games",
-        span:"+$1/mo"
+        img: Check,
+        p: "Online service",
+        p1: "Access to multiplayer games",
+        span: "+$1/mo"
     },
     {
-        img:Check,
-        p:"Larger storage",
-        p1:"Extra 1TB of cloud save",
-        span:"+$2/mo"
+        img: Check,
+        p: "Larger storage",
+        p1: "Extra 1TB of cloud save",
+        span: "+$2/mo"
     },
     {
-        img:Check,
-        p:"Customizable Profile",
-        p1:"Custom theme on your profile",
-        span:"+$2/mo"
+        img: Check,
+        p: "Customizable Profile",
+        p1: "Custom theme on your profile",
+        span: "+$2/mo"
     }
 ]
 
 export const SummaryText = [
     {
-        h3:"Finishing up",
-        p:"Double-check everything looks OK before confirming.",
+        h3: "Finishing up",
+        p: "Double-check everything looks OK before confirming.",
 
 
     }
@@ -107,21 +122,21 @@ export const SummaryText = [
 export const SummaryDiv = [
 
     {
-        h4:"Arcade(Monthly)",
-        u:"change",
-        money0:"$9/mo",
+        h4: "Arcade(Monthly)",
+        u: "change",
+        money0: "$9/mo",
 
     },
     {
-        span:"Online service",
-        money:"+$1/mo",
-        span1:"Larger storage",
-        money1:"+$2/mo",
+        span: "Online service",
+        money: "+$1/mo",
+        span1: "Larger storage",
+        money1: "+$2/mo",
 
     },
     {
-        total:"Total (per month)",
-        tomoney:"$12/mo",
- 
+        total: "Total (per month)",
+        tomoney: "$12/mo",
+
     }
 ]
